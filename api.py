77d3a8751a9e1a6207e5a46fdae1ba134d1fef19
Max <mp7636681@gmail.com>
2024-11-17 23:51:49 +0530

    okay

--- api.py
+++ api.py
@@ -9,7 +9,7 @@ db=SQLAlchemy(app)
 api=Api(app)
 
 
-class Usermodel(db.Model):
+class UserModel(db.Model):
      id=db.Column(db.Integer,primary_key=True)
      name=db.Column(db.String(80),unique=True,nullable=False)
      email=db.Column(db.String(80),unique=True,nullable=False)
